a4441585590116b238f5514832cd19b2219230f8
Steve Hall <oclipa@users.noreply.github.com>
2016-07-27 13:54:20 +0300

    first usable version

---
+++ PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace SteveHall.NotesWindow
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidNotesWindow =    0x101;
+
+    };
+}
